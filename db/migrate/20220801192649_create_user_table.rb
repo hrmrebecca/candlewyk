8e0436f77843c80134fffc0e39bcd005cdb3b338
hhunter1 <haydenhunter123@gmail.com>
2022-08-01 16:59:31 -0400

    added id field to user table

--- db/migrate/20220801192649_create_user_table.rb
+++ db/migrate/20220801192649_create_user_table.rb
@@ -1,6 +1,6 @@
 class CreateUserTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :user_tables do |t|
+    create_table :user_tables, :id,  do |t|
       t.string :first_name
       t.string :last_name
       t.string :username
