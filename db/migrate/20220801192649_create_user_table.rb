e50e430ef0d7fbd01124462799d620e3a99e3b82
hhunter1 <haydenhunter123@gmail.com>
2022-08-02 14:12:06 -0400

    fixed postgres and ran first migration, creating user table

--- db/migrate/20220801192649_create_user_table.rb
+++ db/migrate/20220801192649_create_user_table.rb
@@ -1,7 +1,6 @@
 class CreateUserTable < ActiveRecord::Migration[7.0]
   def change
     create_table :user_tables  do |t|
-      t.integer :id, primary_key: true
       t.string :first_name
       t.string :last_name
       t.string :username
